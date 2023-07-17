ba0b38a825e772d07e419104322f9b5d1033e18a
Bigi-Med <medbigi2000@gmail.com>
2023-07-17 17:42:43 +0200

    refacto

--- src/main/kotlin/com/plugin/Panel/VoiceRecorderPanel.kt
+++ src/main/kotlin/com/plugin/Panel/VoiceRecorderPanel.kt
@@ -7,11 +7,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 import org.jetbrains.annotations.NotNull
 import java.io.*
-import java.util.*
-import javafx.*
 import javax.sound.sampled.*
 import javax.swing.JButton
-import javax.swing.JFileChooser
 import javax.swing.JPanel
 
 class VoiceRecorderPanel : JPanel(), ToolWindowFactory, DumbAware {
@@ -23,18 +20,8 @@ class VoiceRecorderPanel : JPanel(), ToolWindowFactory, DumbAware {
         val startButton = createTextButton("Start")
         val pauseButton = createTextButton("Pause")
         val endButton = createTextButton("End")
-        val playButton = createTextButton("Play")
-        val updateButton = createTextButton("Update")
         startButton.addActionListener {
             startRecording()
-        }
-
-        playButton.addActionListener {
-            playAudioClip()
-        }
-
-        updateButton.addActionListener {
-            updateAudioFile()
         }
 
         pauseButton.addActionListener {
@@ -47,20 +34,6 @@ class VoiceRecorderPanel : JPanel(), ToolWindowFactory, DumbAware {
         add(startButton)
         add(pauseButton)
         add(endButton)
-        add(playButton)
-        add(updateButton)
-    }
-
-    private fun updateAudioFile() {
-        val fileChooser = JFileChooser()
-        val result = fileChooser.showOpenDialog(this)
-
-        if (result == JFileChooser.APPROVE_OPTION) {
-            audioFile = fileChooser.selectedFile
-            println("Updated audio file: $audioFile")
-        } else {
-            println("No audio file selected")
-        }
     }
     private fun startRecording() {
         if (!isRecording) {
@@ -131,57 +104,6 @@ class VoiceRecorderPanel : JPanel(), ToolWindowFactory, DumbAware {
             e.printStackTrace()
         }
     }
-
-    private fun playAudioClip() {
-        try {
-            val audioStream = AudioSystem.getAudioInputStream(audioFile)
-            val format = audioStream.format
-            val supportedFormats = arrayOf("WAV", "AIFF", "AU") // Add more supported formats as needed
-
-            if (!supportedFormats.contains(format.encoding.toString().uppercase(Locale.getDefault()))) {
-                println("Unsupported audio format: ${format.encoding}")
-                return
-            }
-
-
-            val info = DataLine.Info(Clip::class.java, format)
-            val audioClip = AudioSystem.getLine(info) as Clip
-            audioClip.open(audioStream)
-            audioClip.start()
-        } catch (ex: UnsupportedAudioFileException) {
-            println("The specified audio file is not supported.")
-            ex.printStackTrace()
-        } catch (ex: LineUnavailableException) {
-            println("Audio line for playing back is unavailable.")
-            ex.printStackTrace()
-        } catch (ex: IOException) {
-            println("Error playing the audio file.")
-            ex.printStackTrace()
-        }
-    }
-
-
-    private fun verifyAudioCodec(filePath: String): Boolean {
-            val audioFile = File(filePath)
-            val audioInputStream = AudioSystem.getAudioInputStream(audioFile)
-            val audioFormat = audioInputStream.format
-            val audioCodec = audioFormat.encoding.toString()
-
-            // Check if the audio codec is supported
-            val supportedCodecs = listOf("PCM_SIGNED", "PCM_UNSIGNED", "MP3", "FLAC")
-            if (!supportedCodecs.contains(audioCodec)) {
-                println("Unsupported audio codec: $audioCodec")
-                return false
-            }
-
-            // Other checks or processing logic if needed
-
-            return true
-
-    }
-
-
-
     override fun createToolWindowContent(
         @NotNull project: Project,
         @NotNull toolWindow: ToolWindow
